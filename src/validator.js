23c5e660a0484ac1aa4445f76a6ea35f4d5e7fc9
XIOMY-SKAYURI <xiomygarciaguerrero@gmail.com>
2020-08-05 05:28:28 -0500

    nuevas modificaciones

--- src/validator.js
+++ src/validator.js
@@ -1,4 +1,5 @@
 let validator = {
+// OBJETO ISVALID
   isValid: function (numberCard){
     //Convierto el string en array
     let creditCardNumber = numberCard.split("");
@@ -12,15 +13,15 @@ let validator = {
     //Si el número o dígito tiene como residuo 0 entonces lo multiplico por 2
       if(i%2==0){
           orden1=orden*2;
-      // si el resultado es mayor a 9  entonces sumo el residuo mas 1
-         if (orden>9){
+      //Si el resultado es mayor a 9  entonces sumo el residuo mas 1
+        if (orden>9){
            orden1=1+(orden%10);
-        // en caso contrario sólo lo multiplica por 2
+        //En caso contrario sólo lo multiplica por 2
          } else {
            orden2= orden*2;
          }
       }
-    // sumo todos los dígitos
+    //Sumo todos los dígitos
      suma= orden1+orden2+orden;
      if (suma%10==0){
        return true;
@@ -29,60 +30,23 @@ let validator = {
      }
     }
   },
-
+//OBJETO MASKIFY
 maskify: function(numberCard){
-  //se creo un variable resultado
+  //Se creó un variable resultado
   let resultado = '';
-  // recorrer el string
+  //Recorrer el string
   for (let i=0; i< numberCard.length;i++){
         console.log(resultado);
+  //Si la cadena recorrida es mayor o ingual a la longitud de la cadena menos 4
     if (i>=numberCard.length-4){
+  //Entonces se debe arrojar el string recorrido
     resultado += numberCard[i];
+  //En caso contrario reemplazar por #  
   }else{
       resultado += "#"
   }
   }
   console.log(resultado);
-    // console.log del elemento
-    // guardar un michi en el resultado
-
-/*let suma=0;
-let fourDigit=0;
-let digit=0;
-//Declarar longitud del array
-let longitud =0;
-// Definir el número ingreado-- y convertir a array
-let numberIngreso= numberCard.split("");
-//CAPTURAR LOS ÚLTIMOS 4 DÍGITOS
-//Invertir el array
- let arrayinv= numberIngreso.reverse();
-
- //obtener los últimos 4 digitos
- let newArray = arrayinv.splice(0,4,"#","#","#","#");
- newArray.reverse();
-
-fourDigit=  newArray.join('');
-        console.log(newArray);
-
-
-//CAPTURAR LOS DÍGITOS - LOS ÚLTIMOS 4 digitos
-//Creo un bucle que me ayude a recorrer el array
-for (let i=0; i<numberCard.length;i++){
-// Si la longitud de array es mayor a 4
-  if (numberIngreso.length>4){
-// y si el orden de array es mayor o igual a la longitud menos 4
- if (i >= numberIngreso.length-4){
-//entonces extraigo todo el array
-  longitud = numberIngreso.splice(0);
- longitud.reverse();
- digit =longitud.join('');
- console.log(digit);
- }
-
-}
-}
-suma = (digit );
-console.log(suma);*/
 return resultado;
 }
 };
