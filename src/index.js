57e7d2150c966b0d904cd48f0691e3ca10e9fc59
XIOMY-SKAYURI <xiomygarciaguerrero@gmail.com>
2020-08-09 20:02:17 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import validator from './validator.js';
 //Seleccionar el fomrulario del documento por medio del valor del atributo id
-let form = document.getElementById("formulario")
+//let form = document.getElementById("formulario")
 //Seleccionar la tarjeta -visa- por medio del valor del atributo id
-let imgTarjetaVisa = document.getElementById("visa")
+//let imgTarjetaVisa = document.getElementById("visa")
 //Seleccionar la tarjeta - mastercard- por medio del valor del atributo id
-let imgTarjetaMastercard = document.getElementById("mastercard")
+//let imgTarjetaMastercard = document.getElementById("mastercard")
 //Seleccionar el boton de validación del documento por medio del valor del atributo id
 let validarnumber = document.getElementById("validarNumber");
 //Generar un evento para mostar monbres
   validarnumber.addEventListener("click", () => {
 //Seleccionar el nombre del usuario por medio del valor del atributo id
-let nombres = document.getElementById("nombrespaciente").value
+  let nombres = document.getElementById("nombrespaciente").value
 //Declaro una variable que contendrá msm
-let nombresAp = 'HOLA ' + nombres + '!🙋‍♀️';
+  let nombresAp = 'HOLA ' + nombres + '!🙋‍♀️';
 //Colocar el mensaje que se muestra al Usuario llamando su nombre
   document.getElementById("mostrarnombre").innerHTML= nombresAp
   return nombresAp
@@ -22,30 +22,30 @@ let nombresAp = 'HOLA ' + nombres + '!🙋‍♀️';
 //Cancelar el evento pero sin dejar de ejecutar la función
   event.preventDefault();
 //Seleccionar el número ingresado por medio del valor del atributo id
-let numberCard = document.getElementById("numberCard").value;
+  let numberCard = document.getElementById("numberCard").value;
 //Importar el objeto isValid
-let result = validator.isValid(numberCard);
-  console.log(result);
+  let result = validator.isValid(numberCard);
+//  console.log(result);
 //Importar el objeto maskify
-let mascara = validator.maskify(numberCard);
+  let mascara = validator.maskify(numberCard);
 //Colocar el mensaje que se debe mostrar al usuario del HTML contenido en <p> por medio del valor del id
   document.getElementById("mascara").innerHTML= ' *SU '+' TARJETA '+  mascara   +" ES: ";
 //Declaro una nueva variable que será igual al mensaje mostrado al usuario sobre la declaración de la variable
-let texto;
+  let texto;
 //Creo una condicional donde se mostrará el mensaje si es true o false
-if (result==true){
-    texto = "<span style = 'color:green;'> VÁLIDA 😉</span>";
-    document.getElementById("valiNum").innerHTML = texto;
-    return false;
-    }else {
-    texto = "<span style = 'color:red;'>INVÁLIDA 😖</span>";
-    document.getElementById("valiNum").innerHTML = texto;
-    return false;
-    }
-  })
+    if (result==true){
+      texto = "<span style = 'color:green;'> VÁLIDA 😉</span>";
+      document.getElementById("valiNum").innerHTML = texto;
+      return false;
+      }else {
+        texto = "<span style = 'color:red;'>INVÁLIDA 😖</span>";
+        document.getElementById("valiNum").innerHTML = texto;
+        return false;
+      }
+    })
 //Seleccionar el mes de expiración por medio del valor del atributo id
-let mesIngresado = document.getElementById("mesexpiracion")
+//let mesIngresado = document.getElementById("mesexpiracion")
 //Seleccionar el año de expiración por medio del valor del atributo id
-let añoIngresado = document.getElementById("añoexpiracion")
+//let añoIngresado = document.getElementById("añoexpiracion")
 //Seleccionar el cvv  por medio del valor del atributo id
-let cvvIngresado = document.getElementById("cvv")
+//let cvvIngresado = document.getElementById("cvv")
--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,34 @@
-let validator = {
+const validator = {
 // OBJETO ISVALID
   isValid: function (numberCard){
     //Convierto el string en array
     let creditCardNumber = numberCard.split("");
-    let suma = 0;
-    //Creo el bucle
-    for(let i=0; i<creditCardNumber.length;i++){
-    //Convierto el array a números enteros
-      let orden=parseInt(creditCardNumber[i]);
-      let orden1=0;
-      let orden2=0;
-    //Si el número o dígito tiene como residuo 0 entonces lo multiplico por 2
-      if(i%2==0){
-          orden1=orden*2;
-      //Si el resultado es mayor a 9  entonces sumo el residuo mas 1
-        if (orden>9){
-           orden1=1+(orden%10);
-        //En caso contrario sólo lo multiplica por 2
-         } else {
-           orden2= orden*2;
+    //Invierto el array
+    let numberReverse = creditCardNumber.reverse();
+   //Declaro la variable suma
+  let suma=0;
+    //Creo el bucle donse se va a recorrer el array
+  for(let i=0; i<numberReverse.length;i++){
+    //Se declara una variable y se parsea un argumento de tipo cadena devolviendo un entero de la base }
+    let  orden=parseInt(numberReverse[i]);
+    //Obtengo número de posición par
+      if(i % 2 != 0){
+    //Si el número está en posición par lo multiplico por 2
+      orden = orden * 2;
+    //Si el resultado de la multiplicación muestra un número con 2 dígitos
+        if (orden > 9){
+    //Se tiene que sacar el residuo de ese número y sumarle + 1 (es como sumar lo 2 dígitos)
+       orden =  1 + (orden%10);
          }
       }
-    //Sumo todos los dígitos
-     suma= orden1+orden2+orden;
+    //Sumo todos los números y los nuevos
+           suma += orden;
+   }
      if (suma%10==0){
        return true;
      } else {
        return false;
      }
-    }
   },
 //OBJETO MASKIFY
 maskify: function(numberCard){
@@ -36,17 +36,17 @@ maskify: function(numberCard){
   let resultado = '';
   //Recorrer el string
   for (let i=0; i< numberCard.length;i++){
-        console.log(resultado);
+      //  console.log(resultado);
   //Si la cadena recorrida es mayor o ingual a la longitud de la cadena menos 4
     if (i>=numberCard.length-4){
   //Entonces se debe arrojar el string recorrido
     resultado += numberCard[i];
-  //En caso contrario reemplazar por #  
+  //En caso contrario reemplazar por #
   }else{
       resultado += "#"
   }
   }
-  console.log(resultado);
+//  console.log(resultado);
 return resultado;
 }
 };
